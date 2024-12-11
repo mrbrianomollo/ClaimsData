6c05be0a871feae60d4709eb5833ffd2db5dc114
Brian Ongati <88836215+mrbrianomollo@users.noreply.github.com>
2024-12-10 21:00:28 -0500

    Update claims_data_processing.R

--- claims_data_processing.R
+++ claims_data_processing.R
@@ -10,23 +10,58 @@ claims_data <- claims_data %>%
   mutate(
     DxCodes = case_when(
       # Diabetes
-      grepl("^E08|^E09|^E10|^E11|^E13|^O24|^O99.81|", ICDDxCode) ~ "Diabetes",
-      ICDDxCode %in% c("E89.1") ~ "Diabetes",
+      str_detect(ICDDxCode, "^E08") ~ "Diabetes",
+      str_detect(ICDDxCode, "^E09") ~ "Diabetes",
+      str_detect(ICDDxCode, "^E10") ~ "Diabetes",
+      str_detect(ICDDxCode, "^E11") ~ "Diabetes",
+      str_detect(ICDDxCode, "^E13") ~ "Diabetes",
+      ICDDxCode == "E89.1" ~ "Diabetes",
+      str_detect(ICDDxCode, "^O24") ~ "Diabetes",
+      str_detect(ICDDxCode, "^O99.81") ~ "Diabetes",
       
       # High Blood Pressure
-      ICDDxCode %in% c("G93.2", "H40.05", "I20.89", "I21.B") ~ "High Blood Pressure",
-      grepl("^I10|^I11|^I12|^I13|^I15|^I27.0|^I27.2|^I87.3|^I97.3|^K76.6|^O10|^O11|^O13|^O16|^P29.2|^R03.0", ICDDxCode) ~ "High Blood Pressure",
+      ICDDxCode == "G93.2" ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^H40.05") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^I10") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^I11") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^I12") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^I13") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^I15") ~ "High Blood Pressure",
+      ICDDxCode == "I20.89" ~ "High Blood Pressure",
+      ICDDxCode == "I21.B" ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^I27.0") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^I27.2") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^I87.3") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^I97.3") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^K76.6") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^O10") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^O11") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^O13") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^O16") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^P29.2") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^P29.30") ~ "High Blood Pressure",
+      str_detect(ICDDxCode, "^R03.0") ~ "High Blood Pressure",
       
       # COPD and Asthma
-      grepl("^J41|^J42|^J43|^J44|^J45|^J47", ICDDxCode) ~ "COPD and Asthma",
+      str_detect(ICDDxCode, "^J41") ~ "COPD and Asthma",
+      str_detect(ICDDxCode, "^J42") ~ "COPD and Asthma",
+      str_detect(ICDDxCode, "^J43") ~ "COPD and Asthma",
+      str_detect(ICDDxCode, "^J44") ~ "COPD and Asthma",
+      str_detect(ICDDxCode, "^J45") ~ "COPD and Asthma",
+      str_detect(ICDDxCode, "^J47") ~ "COPD and Asthma",
       
       # Default
       TRUE ~ "Other"
     )
   )
 
-# View the first few rows of the cleaned data
-head(claims_data)
+# Check for DxCodes 'Other'
+unmatched_codes <- claims_data %>%
+  filter(DxCodes == "Other") %>%
+  select(ICDDxCode) %>%
+  distinct()
+
+head(unmatched_codes)
 
 # Step 2: Convert Columns
 # List of currency columns
